b7b780b9b034d47018b9046de08a72d894a938a6
Ireneusz Pastusiak <ireneusz.pastusiak@gmail.com>
2024-03-24 20:05:53 +0000

    add hardcoded memory allocation

--- src/memuse.c
+++ src/memuse.c
@@ -1,5 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/mman.h>
 
 int main(void) {
-    puts("hello");
+    puts("start");
+
+    void *memory;
+    size_t size = 1000 * 1024 * 1024;
+    memory = malloc(size);
+    mlock(memory, size);
+
+    if (memory == NULL) {
+        puts("Memory allocation error");
+        exit(1);
+    }
+
+    sleep(10);
+
+    free(memory);
+    memory = NULL;
+
+    puts("end");
 }
