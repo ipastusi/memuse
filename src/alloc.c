2476e46b1ae95b75304acecd4f7e27053e17e067
Ireneusz Pastusiak <ireneusz.pastusiak@gmail.com>
2024-03-31 10:56:08 +0100

    improve memory cleanup

--- src/alloc.c
+++ src/alloc.c
@@ -8,15 +8,21 @@
 static alloc_unit *cfg;
 static void *memory;
 static bool allocated = false;
+static bool allocated_cfg = false;
 
 static int allocate(unsigned int size, unsigned int sec, bool is_mb);
 
 static void unallocate(void);
 
+static void cleanup(void);
+
 int run(const char *const cfg_str, const bool is_mb, const bool wrap) {
     cfg = parse(cfg_str, wrap);
     if (cfg == NULL) return EXIT_FAILURE;
+    allocated_cfg = true;
     const alloc_unit *current = cfg;
+
+    atexit(cleanup);
 
     int alloc_res;
     while (true) {
@@ -31,14 +37,12 @@ int run(const char *const cfg_str, const bool is_mb, const bool wrap) {
         }
     }
     unallocate_cfg(cfg);
+    allocated_cfg = false;
     return (alloc_res != 0) ? EXIT_FAILURE : EXIT_SUCCESS;
 }
 
 void int_handler(const int sig) {
     fprintf(stderr, "\nreceived signal: %d\n", sig);
-    if (allocated) unallocate();
-    unallocate_cfg(cfg);
-    fprintf(stderr, "exiting...\n");
     exit(EXIT_FAILURE);
 }
 
@@ -63,8 +67,15 @@ static int allocate(const unsigned int size, const unsigned int sec, const bool 
 }
 
 static void unallocate(void) {
+    if (allocated != true) return;
     printf("unallocating memory\n");
     free(memory);
     memory = NULL;
     allocated = false;
+}
+
+static void cleanup(void) {
+    unallocate();
+    if (allocated_cfg) unallocate_cfg(cfg);
+    printf("exiting...\n");
 }
